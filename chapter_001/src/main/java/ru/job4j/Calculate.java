9f1b79b9f08eaa05cf2e1283e4e84b1af2e91a4f
ovchinnikovanton <ant261081@gmail.com>
2017-10-13 14:06:29 +0300

    Сделал задачу [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,14 @@
 package ru.job4j;
+/**
+* Class для начала
+* @author Anton Ovchinnikov
+* @since 13.10.2017
+* @version 1.0
+*/
 public class Calculate {
+	/** Вывод строки в консоль
+	* @param arg - arg
+	*/
 	public static void main (String[] args) {
 		System.out.println ("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Anton Ovchinnikov (mailto:ant261081@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
