3371b2c159dbd53cdff8b1db3c6ca266ad54288e
ovchinnikovanton <ant261081@gmail.com>
2017-10-16 16:26:01 +0300

    1. Настройка IDEA [#4417]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,23 +1,23 @@
 package ru.job4j;
 /**
-* Class для начала.
-* @author Anton Ovchinnikov.
-* @since 13.10.2017.
-* @version 1.0.
-*/
+ * Class для начала.
+ * @author Anton Ovchinnikov.
+ * @since 13.10.2017.
+ * @version 1.0.
+ */
 public class Calculate {
 	/** Вывод строки в консоль.
-	* @param args args.
-	*/
+	 * @param args args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 	/**
-* Method echo.
-* @param name Anton Ovchinnikov.
-* @return Echo plus Anton Ovchinnikov.
-*/
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
-}
+	 * Method echo.
+	 * @param name Anton Ovchinnikov.
+	 * @return Echo plus Anton Ovchinnikov.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
